ae99f0293fc6133458ef258ca375ae196300d299
Yeobie <konoha.shoujo@gmail.com>
2023-02-03 23:58:03 +0100

    Added eventlistener events to the accordion images

---
+++ index.js
@@ -0,0 +1,126 @@
+let namcounter = 1;
+
+let nambutton = document.querySelector("#nambutton");
+nambutton.addEventListener("click", function () {
+  let nam = document.querySelector("#nam");
+  nam.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/695/original/2nam.png?1675455014"
+  );
+  namcounter++;
+  if (namcounter > 2) {
+    namcounter = 1;
+    nam.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/669/original/1Nam.png?1675445283"
+    );
+  }
+});
+
+let jincounter = 1;
+let jinbutton = document.querySelector("#jinbutton");
+jinbutton.addEventListener("click", function () {
+  let jin = document.querySelector("#jinnie");
+  jin.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/701/original/2jin.png?1675457456"
+  );
+  jincounter++;
+  if (jincounter > 2) {
+    jincounter = 1;
+    jin.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/668/original/1Jin.png?1675445278"
+    );
+  }
+});
+
+let sugacounter = 1;
+let sugabutton = document.querySelector("#sugabutton");
+sugabutton.addEventListener("click", function () {
+  let suga = document.querySelector("#suga");
+  suga.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/717/original/2yoongi.png?1675463798"
+  );
+  sugacounter++;
+  if (sugacounter > 2) {
+    sugacounter = 1;
+    suga.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/667/original/1Yoongi.png?1675445271"
+    );
+  }
+});
+
+let hobicounter = 1;
+let hobibutton = document.querySelector("#hobibutton");
+hobibutton.addEventListener("click", function () {
+  let hobi = document.querySelector("#hobi");
+  hobi.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/718/original/2hobi.png?1675463961"
+  );
+  hobicounter++;
+  if (hobicounter > 2) {
+    hobicounter = 1;
+    hobi.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/666/original/1hobi.png?1675445265"
+    );
+  }
+});
+
+let mimiicounter = 1;
+let jiminbutton = document.querySelector("#jiminbutton");
+jiminbutton.addEventListener("click", function () {
+  let jimin = document.querySelector("#jimin");
+  jimin.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/719/original/2jimin.png?1675464261"
+  );
+  mimicounter++;
+  if (mimicounter > 2) {
+    mimicounter = 1;
+    jimin.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/665/original/1jimin.png?1675445258"
+    );
+  }
+});
+
+let tetecounter = 1;
+let tetebutton = document.querySelector("#tetebutton");
+tetebutton.addEventListener("click", function () {
+  let tete = document.querySelector("#tete");
+  tete.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/721/original/2tae.png?1675464436"
+  );
+  tetecounter++;
+  if (tetecounter > 2) {
+    tetecounter = 1;
+    tete.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/664/original/1tae.png?1675445252"
+    );
+  }
+});
+
+let jkcounter = 1;
+let kookiebutton = document.querySelector("#kookiebutton");
+kookiebutton.addEventListener("click", function () {
+  let kookie = document.querySelector("#kookie");
+  kookie.setAttribute(
+    "src",
+    "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/722/original/2jk.png?1675464899"
+  );
+  jkcounter++;
+  if (jkcounter > 2) {
+    jkcounter = 1;
+    kookie.setAttribute(
+      "src",
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/065/663/original/1jk.png?1675445244"
+    );
+  }
+});
